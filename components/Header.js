46b273f2029fcdf518d7798be9e82c90b862ec62
Kan <kan1657@hotmail.com>
2021-11-19 13:40:14 +0700

    Finish Stories

--- components/Header.js
+++ components/Header.js
@@ -13,7 +13,7 @@ import { HomeIcon } from "@heroicons/react/solid";
 
 function Header() {
   return (
-    <div>
+    <div className="shadow-sm border-b bg-white sticky top-0 z-50">
       <div className="flex justify-between max-w-6xl mx-5 xl:mx-auto items-center ">
         {/* Left - Logo */}
         <div className="relative w-24 h-24 hidden lg:inline-grid cursor-pointer">
